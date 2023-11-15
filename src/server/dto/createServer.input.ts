66c06807ecae16a27721cbf8f5da93f93e4ab1f2
Masih Jahangiri <masihjahan@gmail.com>
2023-11-15 20:10:38 +0330

    feat: remove validation for add server domain

--- src/server/dto/createServer.input.ts
+++ src/server/dto/createServer.input.ts
@@ -8,7 +8,7 @@ export class CreateServerInput {
   ip: string;
 
   @Field()
-  @Matches(/^[\dA-Za-z][\dA-Za-z-]{1,61}[\dA-Za-z]\.[A-Za-z]{1,10}$/)
+  // @Matches(/^[\dA-Za-z][\dA-Za-z-]{1,61}[\dA-Za-z]\.[A-Za-z]{1,10}$/)
   domain: string;
 
   @Field(() => ServerCountry)
