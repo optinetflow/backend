2ec365e32be91d009ecd999b88fd26c0c46939ed
Masih Jahangiri <masihjahan@gmail.com>
2023-12-07 17:13:33 +0330

    feat: change traffic to float in GQL

--- src/xui/models/package.model.ts
+++ src/xui/models/package.model.ts
@@ -1,12 +1,12 @@
 import 'reflect-metadata';
 
-import { Field, Int, ObjectType } from '@nestjs/graphql';
+import { Field, Float, Int, ObjectType } from '@nestjs/graphql';
 
 import { BaseModel } from '../../common/models/base.model';
 
 @ObjectType()
 export class Package extends BaseModel {
-  @Field(() => Int)
+  @Field(() => Float)
   traffic: number;
 
   @Field(() => Int)
