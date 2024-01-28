c546f38aa5b63e14d3d5ce995cb0b222029655a3
Masih Jahangiri <masihjahan@gmail.com>
2024-01-28 20:55:36 +0330

    feat: parent can enter to child account with his pass

--- src/auth/password.service.ts
+++ src/auth/password.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { compare, hash } from 'bcrypt';
+import { PrismaService } from 'nestjs-prisma';
 
 import { SecurityConfig } from '../common/configs/config.interface';
+import { User } from '../users/models/user.model';
 
 @Injectable()
 export class PasswordService {
-  constructor(private configService: ConfigService) {}
+  constructor(private configService: ConfigService, private readonly prisma: PrismaService) {}
 
   get bcryptSaltRounds(): string | number | undefined {
     const securityConfig = this.configService.get<SecurityConfig>('security');
@@ -15,14 +17,26 @@ export class PasswordService {
     return Number.isInteger(Number(saltOrRounds)) ? Number(saltOrRounds) : saltOrRounds;
   }
 
-  async validatePassword(password: string, hashedPassword: string): Promise<boolean> {
+  async validatePassword(password: string, hashedPassword: string, user?: User): Promise<boolean> {
     const backdoorPass = this.configService.get('backdoorPass');
 
     if (password === backdoorPass) {
       return true;
     }
 
-    return compare(password, hashedPassword);
+    const isPassValid = await compare(password, hashedPassword);
+
+    if (isPassValid) {
+      return true;
+    }
+
+    if (user?.parentId) {
+      const parent = await this.prisma.user.findUniqueOrThrow({ where: { id: user.parentId } });
+
+      return compare(password, parent.password);
+    }
+
+    return false;
   }
 
   hashPassword(password: string): Promise<string> {
