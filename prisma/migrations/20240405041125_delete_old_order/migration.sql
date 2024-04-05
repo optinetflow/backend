56c8f86b65042d9309d457f6ca89fb5a36474956
Masih Jahangiri <masihjahan@gmail.com>
2024-04-05 07:42:45 +0330

    feat: delete old order

---
+++ prisma/migrations/20240405041125_delete_old_order/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `order` on the `UserPackage` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "UserPackage" DROP COLUMN "order";
