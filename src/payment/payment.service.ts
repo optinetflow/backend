f8ee6bc13bb8c168689d6fa5ce55f9a9f2be0038
Masih Jahangiri <35561271+masihjahangiri@users.noreply.github.com>
2024-05-20 15:24:52 +0330

    Improve order of recharge package

--- src/payment/payment.service.ts
+++ src/payment/payment.service.ts
@@ -46,6 +46,9 @@ export class PaymentService {
           lte: user.maxRechargeDiscountPercent || 50,
         },
       },
+      orderBy: {
+        discountPercent: 'asc',
+      },
     });
   }
 
