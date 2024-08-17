055163f00b16e76dbba0e152be8b85fbdb7a900a
ho3einmolavi <ho3einmolavi00@gmail.com>
2024-08-17 19:00:47 +0430

    add fullname to users

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ import { PrismaClientExceptionFilter } from 'nestjs-prisma';
 
 import { AppModule } from './app.module';
 import type { CorsConfig, NestConfig, SwaggerConfig } from './common/configs/config.interface';
+import { AppService } from './app.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -50,6 +51,9 @@ async function bootstrap() {
     app.enableCors();
   }
 
+  const appService = app.get(AppService);
+  // Run the populateFullname method during bootstrapping
+  await appService.populateFullname();
   await app.listen(process.env.PORT || nestConfig?.port || 3000);
 }
 
