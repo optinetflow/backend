f447cd0f6331d3c81b3bf5314e5d7c5eb1d0ce20
ho3einmolavi <ho3einmolavi00@gmail.com>
2024-08-30 18:42:38 +0430

    feat: populateTelegramUserChatId

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'nestjs-prisma';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly prisma: PrismaService) {}
 
   getHello(): string {
     return 'Hello World!';
@@ -9,5 +11,21 @@ export class AppService {
 
   getHelloName(name: string): string {
     return `Hello ${name}!`;
+  }
+
+  async populateTelegramUserChatId() {
+    const telegramUsers = await this.prisma.telegramUser.findMany();
+    const promises = telegramUsers.map(async (telegramUser) =>
+      this.prisma.telegramUser.update({
+        where: {
+          id: telegramUser.id,
+        },
+        data: {
+          chatId: telegramUser.id,
+        },
+      }),
+    );
+
+    return Promise.all(promises);
   }
 }
