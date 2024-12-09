fa2bd5b22ac5015d2996756820b0d908b2bd7975
ho3einmolavi <ho3einmolavi00@gmail.com>
2024-12-09 05:30:19 +0330

    update getChildren

---
+++ src/users/enums/user-segment.enum.ts
@@ -0,0 +1,8 @@
+export enum UserSegment {
+  ENGAGED_SUBSCRIBERS = 'engagedSubscribers',
+  DORMANT_SUBSCRIBERS = 'dormantSubscribers',
+  LONG_LOST_CUSTOMERS = 'longLostCustomers',
+  RECENTLY_LAPSED_CUSTOMERS = 'recentlyLapsedCustomers',
+  NEW_PROSPECTS = 'newProspects',
+  UNCATEGORIZED = 'uncategorized',
+}
