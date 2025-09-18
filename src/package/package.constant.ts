aa443641d6a3bd8517c3852e7183a13d1759194c
Masih Jahangiri <masihjahan@gmail.com>
2025-09-19 03:20:04 +0330

    fix: update bundleGroupSizes to reflect correct discount structure by changing bundleGroupSize from 4 to 5

--- src/package/package.constant.ts
+++ src/package/package.constant.ts
@@ -1,5 +1,5 @@
 export const bundleGroupSizes = [
   { bundleGroupSize: 2, discount: 0.7 },
   { bundleGroupSize: 3, discount: 0.8 },
-  { bundleGroupSize: 4, discount: 0.9 },
+  { bundleGroupSize: 5, discount: 0.9 },
 ];
