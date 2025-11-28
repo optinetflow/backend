1d2316b4db94bee2ef91244500097e2429accfad
Masih Jahangiri <masihjahan@gmail.com>
2025-11-28 13:22:42 +0330

    fix: update discount for bundle group size 2 in package.constants.ts from 0.6 to 0.5

--- src/package/package.constant.ts
+++ src/package/package.constant.ts
@@ -1,5 +1,5 @@
 export const bundleGroupSizes = [
-  { bundleGroupSize: 2, discount: 0.6 },
+  { bundleGroupSize: 2, discount: 0.5 },
   { bundleGroupSize: 3, discount: 0.8 },
   { bundleGroupSize: 5, discount: 0.9 },
 ];
